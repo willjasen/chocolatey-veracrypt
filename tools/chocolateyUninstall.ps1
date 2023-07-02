d2dc0d5a982eab1f8ed73784eb5870dceb4d127d
Brandon <brandon@willjasen.com>
2023-07-01 21:11:40 -0400

    uninstall script now works

--- tools/chocolateyUninstall.ps1
+++ tools/chocolateyUninstall.ps1
@@ -1,9 +1,10 @@
 $packageName = "veracrypt"
 $fileType = "msi"
-$args = "\qn"
+$args = "/qn"
 
+# Get the GUID for VeraCrypt from the registry so it can be uninstalled
 $uninstallGUID = Get-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*" | Where DisplayName -Like "VeraCrypt*" | Select-Object -ExpandProperty PSChildName
 
-$args = $uninstallGUID + $args
+$args = $uninstallGUID + " " + $args
 
 Uninstall-ChocolateyPackage $packageName $fileType $args $setupMsiPath
