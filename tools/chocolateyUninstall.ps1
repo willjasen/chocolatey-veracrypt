f26e959ee01b0569dba95ef7df03a1c1a640f5d7
Brandon <brandon@willjasen.com>
2023-07-01 20:23:20 -0400

    uninstall msi using GUID

--- tools/chocolateyUninstall.ps1
+++ tools/chocolateyUninstall.ps1
@@ -1,28 +1,9 @@
 $packageName = "veracrypt"
-$fileType = "exe"
-$args = "/u"
+$fileType = "msi"
+$args = "\qn"
 
-$is64bit = Get-ProcessorBits 64
+$uninstallGUID = Get-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*" | Where DisplayName -Like "VeraCrypt*" | Select-Object -ExpandProperty PSChildName
 
-if ($is64bit) {
-  $setupExePath = (Get-ItemProperty HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\VeraCrypt).UninstallString.split('"')[1]
-}
-else {
-  $setupExePath = (Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\VeraCrypt).UninstallString.split('"')[1]
-}
+$args = $uninstallGUID + $args
 
-#Thanks to dtgm and the GitHub package for ideas.
-$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-$ahkFile = Join-Path $scriptPath "veracryptUninstall.ahk"
-$ahkRun = "$Env:Temp\$(Get-Random).ahk"
-
-Copy-Item $ahkFile "$ahkRun" -Force
-$ahkProc = Start-Process -FilePath 'AutoHotKey' `
-					   -ArgumentList "`"$ahkRun`"" `
-					   -PassThru
-Write-Debug "$ahkRun start time:`t$($ahkProc.StartTime.ToShortTimeString())"
-Write-Debug "$ahkRun process ID:`t$($ahkProc.Id)"
-
-Uninstall-ChocolateyPackage $packageName $fileType $args $setupExePath
-
-Remove-Item "$ahkRun" -Force
+Uninstall-ChocolateyPackage $packageName $fileType $args $setupMsiPath
